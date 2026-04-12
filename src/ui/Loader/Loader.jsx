c7ca35c009fb7ac770654572aca7c4e7ca5e946a
Mohammad-AbuShehada <mohammadabushehada2@gmail.com>
2026-04-12 15:53:27 +0200

    improved layout and remove unused Category component

--- src/ui/Loader/Loader.jsx
+++ src/ui/Loader/Loader.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress,Box } from "@mui/material";
 export default function Loader() {
-    return  <CircularProgress /> 
+    return  (
+        <Box sx={{display:"flex",justifyContent:"center",alignItems:"center",py:6}}>
+            <CircularProgress />
+        </Box>
+    ) 
 }
